a57f0190896ba9cdd81ed253b81455e16cacd144
Shashi <shekhar.shashi735@gmail.com>
2026-06-23 08:03:00 +0530

    Pagination and sorting feature added

--- Authentication/controllers/image-controller.js
+++ Authentication/controllers/image-controller.js
@@ -46,11 +46,27 @@ const uploadImageController = async (req, res) => {
 
 
 const fetchImagesController = async (req, res) => {
+    
     try {
-        const images = await Image.find({});
-        if (images) {
+    const page = parseInt(req.query.page) || 1
+    const limit = parseInt(req.query.limit) || 5
+    const skip = (page -1)* limit  ;
+
+    const sort = req.query.sortBy || "createdAt";
+    const sortOrder = req.query.sortOrder === "asc" ? 1 :-1;
+    const totalImages = await Image.countDocuments();
+    const totalPages = Math.ceil(totalImages/limit);
+
+    const sortObj = {}
+    sortObj[sortBy] = sortOrder
+    const images = await Image.find().sort(sortObj).skip(skip).limit(limit);
+        
+    if (images) {
             res.status(200).json({
                 success: true,
+                currentPage : page,
+                totalPages : totalPages,
+                totalImages : totalImages,
                 data: images,
             })
         }
